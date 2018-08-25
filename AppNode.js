59eb7648fd9f35fab5f1bce9e855866b20a71fdc
Nanduuu <Nanduuu@users.noreply.github.com>
2018-08-25 18:40:48 +0530

    commit

---
+++ AppNode.js
@@ -0,0 +1,43 @@
+//var nodemailer = require('nodemailer');
+var express = require('express');
+var app = express();
+var router = express.Router();
+var bodyParser = require('body-parser');
+var routers = require('./email');
+
+var jsonParser  = bodyParser.json();
+var urlencodedparser = bodyParser.urlencoded({extended:true});
+
+
+
+
+var port = process.env.PORT || 8080;
+
+var http= require('http');
+app.use(express.static('public/build'));
+app.use('/email/',routers);
+app.use(bodyParser.json());
+//app.use(urlencodedparser);
+
+
+
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static filess
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+
+
+var server = http.createServer(app);
+
+server.listen(port);
+
+
+
+
+
+
